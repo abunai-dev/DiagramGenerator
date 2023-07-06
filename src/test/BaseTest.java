b18158a6481ac01e82149a8c54c26a4738e9bf23
Tizian Bitschi <tizian.bitschi@protonmail.com>
2023-07-06 10:49:36 +0200

    feat: add base test to run generator through junit tests

--- src/test/BaseTest.java
+++ src/test/BaseTest.java
@@ -1,9 +1,6 @@
 package test;
 
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.TestInstance;
-import org.junit.jupiter.api.TestInstance.Lifecycle;
 import org.palladiosimulator.dataflow.diagramgenerator.DataFlowGraphProcessor;
 import org.palladiosimulator.dataflow.diagramgenerator.GeneratorOptions;
 import org.palladiosimulator.dataflow.diagramgenerator.StandaloneDiagramGenerator;
@@ -11,25 +8,25 @@ import org.palladiosimulator.dataflow.diagramgenerator.model.DataFlowElementFact
 import org.palladiosimulator.dataflow.diagramgenerator.plantuml.PlantUMLDrawingStrategy;
 
 public class BaseTest {
-	public static String TEST_MODEL_PROJECT_NAME = "org.palladiosimulator.dataflow.confidentiality.analysis.testmodels";
-
-	private static StandaloneDiagramGenerator diagramGenerator;
-
-	@BeforeAll
-	public static void initializeGenerator() {
-		GeneratorOptions options = GeneratorOptions.getInstance();
-		options.setProjectName(TEST_MODEL_PROJECT_NAME);
-		options.setAllocationPath("models/CoronaWarnApp/default.usagemodel");
-		options.setUsageModelPath("models/CoronaWarnApp/default.allocation");
-
-		diagramGenerator = new StandaloneDiagramGenerator(options);
-	}
+	public static String PROJECT_NAME = "org.palladiosimulator.dataflow.confidentiality.analysis.testmodels";
+	public static String USAGE_MODEL_PATH = "models/CoronaWarnApp/default.usagemodel";
+	public static String ALLOCATION_PATH = "models/CoronaWarnApp/default.allocation";
+	public static String CHARACTERISTICS_PATH = "models/CoronaWarnApp/default.nodecharacteristics";
 
 	@Test
 	void runGeneratorWithCoronaWarnApp() {
+		GeneratorOptions options = GeneratorOptions.getInstance();
+		options.setProjectName(PROJECT_NAME);
+		options.setUsageModelPath(USAGE_MODEL_PATH);
+		options.setAllocationPath(ALLOCATION_PATH);
+		options.setCharacteristicsPath(CHARACTERISTICS_PATH);
+
+		StandaloneDiagramGenerator diagramGenerator = new StandaloneDiagramGenerator(options);
+
 		PlantUMLDrawingStrategy drawer = new PlantUMLDrawingStrategy();
 		DataFlowElementFactory creator = DataFlowElementFactory.getInstance();
 		DataFlowGraphProcessor processor = new DataFlowGraphProcessor(creator);
+
 		diagramGenerator.generateDataFlowDiagram(drawer, creator, processor);
 	}
 }
